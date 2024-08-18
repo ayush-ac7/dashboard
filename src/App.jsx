4bea9f3325966251880c07cecb3251d5b74255df
Ayush Chauhan <ayushchauhan212@gmail.com>
2024-08-18 13:40:21 +0530

    added all files

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="italic text-4xl text-red-400 font-bold  text-center my-40">
+        Welcome back mate!!!
+      </h1>
+    </>
+  );
+}
+
+export default App;
