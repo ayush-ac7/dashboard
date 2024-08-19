a3bdf8a3a08f93edfee772e1072605bbf962bd3a
Ayush Chauhan <ayushchauhan212@gmail.com>
2024-08-19 12:20:19 +0530

    created json data

---
+++ src/dashboardData.js
@@ -0,0 +1,15 @@
+const dashboardData = [
+  {
+    categoryName: "CSM Executive Dashboard",
+    widgets: [
+      { id: 1, name: "Widget 1", text: "Random text 1" },
+      { id: 2, name: "Widget 2", text: "Random text 2" },
+    ],
+  },
+  {
+    categoryName: "Operations Dashboard",
+    widgets: [{ id: 3, name: "Widget 3", text: "Random text 3" }],
+  },
+];
+
+export default dashboardData;
